3689deed48cb7589e3aff4db486ec8b5a2f0dd17
mikumimiestu <xenzadigital@gmail.com>
2025-02-27 10:36:52 +0700

    update login dan register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,22 @@
 const Login = () => {
-    return (
-      <div className="max-w-lg mx-auto bg-white p-8 rounded shadow-md mt-20">
-        <h2 className="text-2xl mb-4 text-center">Login</h2>
+  return (
+    <div className="min-h-screen flex items-center justify-center pt-20">
+      <div className="max-w-lg w-full border p-8 rounded-3xl shadow-md">
+        <h2 className="text-2xl mb-4 text-center text-slate-100 font-semibold">Login</h2>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="email">Email</label>
-            <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded" placeholder="Email" />
+            <label className="block text-gray-200" htmlFor="email">Email</label>
+            <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded-xl" placeholder="Email" />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="password">Password</label>
-            <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded" placeholder="Password" />
+            <label className="block text-gray-200" htmlFor="password">Password</label>
+            <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded-xl" placeholder="Password" />
           </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded">Login</button>
+          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-xl">Login</button>
         </form>
       </div>
-    );
-  };
-  
-  export default Login;
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,24 +1,26 @@
 const Register = () => {
-    return (
-      <div className="max-w-lg mx-auto bg-white p-8 rounded shadow-md mt-20">
-        <h2 className="text-2xl mb-4 text-center">Register</h2>
+  return (
+    <div className="min-h-screen flex items-center justify-center pt-20">
+      <div className="max-w-lg border w-full p-8 rounded-3xl shadow-md">
+        <h2 className="text-2xl mb-4 text-center text-gray-100 font-semibold">Register</h2>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="name">Name</label>
-            <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded" placeholder="Name" />
+            <label className="block text-gray-200" htmlFor="name">Name</label>
+            <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded-xl" placeholder="Name" />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="email">Email</label>
-            <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded" placeholder="Email" />
+            <label className="block text-gray-200" htmlFor="email">Email</label>
+            <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded-xl" placeholder="Email" />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="password">Password</label>
-            <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded" placeholder="Password" />
+            <label className="block text-gray-200" htmlFor="password">Password</label>
+            <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded-xl" placeholder="Password" />
           </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded">Register</button>
+          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-xl">Register</button>
         </form>
       </div>
-    );
-  };
-  
-  export default Register;
+    </div>
+  );
+};
+
+export default Register;
